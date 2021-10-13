f2829e8a930a890461478af844c8bafec2b27682
tetsuyaohira <tetsuya.ohira@icloud.com>
2021-10-13 19:25:33 +0900

    first commit

---
+++ cypress/integration/flier_download.js
@@ -0,0 +1,69 @@
+describe('download flier pdf', () => {
+    beforeEach(() => {
+        login();
+    });
+
+    it('download flier pdf', () => {
+        for (let i = Cypress.env('start_index'); i < Cypress.env('last_index'); i++) {
+            cy.log('No:' + i.toString());
+            download(i);
+        }
+    });
+});
+
+function login() {
+    cy.visit(Cypress.env('login_url'), {});
+    cy.title()
+        .should('include', '本の要約サイト flier(フライヤー)')
+
+    cy.get('#email')
+        .click()
+        .type(Cypress.env('login_id'))
+        .should('have.value', Cypress.env('login_id'));
+
+    cy.get('#password')
+        .click()
+        .type(Cypress.env('login_password'))
+        .should('have.value', Cypress.env('login_password'));
+
+    cy.get('.center > .btn-fl-submit').click();
+
+    cy.wait(1000);
+}
+
+function download(i) {
+
+    cy.visit(`https://www.flierinc.com/summary/${i}`, {});
+
+    cy.url()
+        .then((value) => {
+            if (value.indexOf('summary') === -1) return; // Not found contents
+
+            cy.get('.btn-fl-pdf')
+                .click(); // move view pdf
+
+            cy.url()
+                .then((value) => {
+                        if (value.indexOf('pdfviewer') === -1) return;
+
+                        cy.title()
+                            .then((value) => {
+                                const title = value.substring(0, value.lastIndexOf(' - PDF Viewer')) + '.pdf';
+
+                                cy.get('script')
+                                    .last()
+                                    .then((value) => {
+                                        const start = value.text().indexOf("'") + 1;
+                                        const end = value.text().lastIndexOf("'");
+                                        const url = value.text().substring(start, end);
+                                        cy.request({url: url, encoding: 'binary'})
+                                            .then((response) => {
+                                                const filePath = 'cypress/downloads/' + title;
+                                                cy.writeFile(filePath, response.body, 'binary');
+                                            });
+                                    });
+                            });
+                    }
+                );
+        });
+}
